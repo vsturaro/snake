30596633cc26c0927e892b9391b842d3eca9f3ca
Vander <vstecnologia@gmail.comr>
2020-10-21 20:55:09 -0400

    finalizando estilos

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ let food = {  //criando a comida aleatoriamente
 }
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "lightblue";
     context.fillRect(0 ,0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
 for(i=0; i < snake.length; i++){
-    context.fillStyle = "green";
+    context.fillStyle = "black";
     context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
